dad2ccba1f84dcd6249bacf47a2d9786fff31c9d
Carlo Caprini <7250891+carlocaprini@users.noreply.github.com>
2021-02-04 17:13:00 +0100

    Fixed broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('docs/pilot/intro')}>
               Get Started
             </Link>
           </div>
